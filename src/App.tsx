b0f3e5439a59a40234d253af7c41301976938205
ndoherty-eth <whitevans.eth@gmail.com>
2023-09-10 21:41:40 -0600

    simplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
-import './App.css';
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home';
-import { Project, projects } from './projects';
-import ProjectPage from './pages/ProjectPage/ProjectPage';
-import GreenwoodPage from './pages/Greenwood/GreenwoodPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-
-        { projects.map((project: Project, index: number) => {
-          return (
-            <Route 
-              path={project.slug} 
-              element={
-                project.slug === 'greenwood-labs' 
-                  ? <GreenwoodPage /> 
-                  : <ProjectPage project={project} key={`project-route-${index}`}/>
-              } 
-            />
-          )
-        })}
-      </Routes> 
+      </Routes>
     </BrowserRouter>
 
   );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,88 +1,38 @@
 import React, { useState } from 'react';
-import { ArrowLeft, ArrowRight, GitHub, Mail, PenTool, Twitter } from 'react-feather';
-import { useNavigate } from 'react-router-dom';
-
-import { projects } from '../../projects';
+import { GitHub, Mail, PenTool, Twitter } from 'react-feather';
 
 import './Home.scss';
 
 const Home = () => {
-  const navigate = useNavigate();
-  const [activeProjectIndex, setActiveProjectIndex] = useState<number>(0);
   const [windowHeight, setWindowHeight] = useState<number>(window.innerHeight)
 
   window.addEventListener('resize', () => {
     setWindowHeight(window.innerHeight)
   })
 
-
-  let activeProject = projects[activeProjectIndex] 
-
   return (
-    <div className='home' style={{height: windowHeight}}>
+    <div className='home' style={{ height: windowHeight }}>
       <div className='home-top'>
-          <span>ndoherty.xyz</span>
-          <div>
-            <Mail size={14} strokeWidth={2.5} onClick={() => window.open('mailto:ndoherty.xyz@gmail.com', '_blank')} />
-            <Twitter size={14} strokeWidth={2.5} onClick={() => window.open('https://twitter.com/ndoherty_xyz', '_blank')} />
-            <GitHub size={14} strokeWidth={2.5} onClick={() => window.open('https://github.com/ndoherty-xyz', '_blank')} />
-            <PenTool size={14} strokeWidth={2.25} onClick={() => window.open('https://mirror.xyz/ndoherty.eth', '_blank')} />
-          </div>
-      </div>
+        <div>
+          <span>
+            I'm Nick
+          </span>
+          <span>
+            Currently a frontend engineer at <a target='_blank' rel="noreferrer" href="https://mirror.xyz">Mirror</a>
+          </span>
+          <span>
+            I also do some freelance web design and development with <a target='_blank' rel="noreferrer" href="https://mirror.xyz">RDD</a>
+          </span>
+        </div>
 
-      <div className='home-center'>
-          <img className='home-img' src={activeProject.headerImage} alt={`${activeProject.title} on a laptop screen.`} />
-          <div className='home-center-project'>
-            <ArrowLeft 
-              size={30} 
-              strokeLinecap={undefined} 
-              strokeLinejoin={undefined} 
-              onClick={() => {
-                let newIndex = activeProjectIndex - 1;
-                newIndex < 0 ? setActiveProjectIndex(projects.length - 1) : setActiveProjectIndex(newIndex)
-              }}
-            />
-            <div className='home-center-project-text' onClick={() => navigate(activeProject.slug)}>
-              <span className='home-center-project-text-title'>{activeProject.title}</span>
-              <span> ✱ {activeProject.subtitle}, {activeProject.year}</span>
-            </div>
-            <ArrowRight 
-              size={30} 
-              strokeLinecap={undefined} 
-              strokeLinejoin={undefined}  
-              onClick={() => {
-                let newIndex = activeProjectIndex + 1;
-                newIndex > projects.length - 1 ? setActiveProjectIndex(0) : setActiveProjectIndex(newIndex)
-              }}
-            />
-          </div>
-          {/* <div className='home-center-project-info' onClick={() => navigate(activeProject.slug)}>
-            <span className='home-center-project-info-title'>{activeProject.title}</span>
-            <span>{activeProject.year}</span>
-          </div>
-          <div className='home-center-stepper'>
-          <span 
-              className='arrow' 
-              onClick={() => {
-                let newIndex = activeProjectIndex - 1;
-                newIndex < 0 ? setActiveProjectIndex(projects.length - 1) : setActiveProjectIndex(newIndex)
-              }}
-            >
-              ←
-            </span>
-            <span>{activeProjectIndex + 1} of {projects.length}</span>
-            <span 
-              className='arrow' 
-              onClick={() => {
-                let newIndex = activeProjectIndex + 1;
-                newIndex > projects.length - 1 ? setActiveProjectIndex(0) : setActiveProjectIndex(newIndex)
-              }}
-            >
-              →
-            </span>
-          </div> */}
+        <div>
+          <Mail size={18} strokeWidth={2.5} onClick={() => window.open('mailto:ndoherty.xyz@gmail.com', '_blank')} />
+          <Twitter size={18} strokeWidth={2.5} onClick={() => window.open('https://twitter.com/ndoherty_xyz', '_blank')} />
+          <GitHub size={18} strokeWidth={2.5} onClick={() => window.open('https://github.com/ndoherty-xyz', '_blank')} />
+          <PenTool size={18} strokeWidth={2.25} onClick={() => window.open('https://nick.mirror.xyz', '_blank')} />
+        </div>
       </div>
-    </div>
+    </div >
   )
 }
 
